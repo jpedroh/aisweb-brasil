abd48060b2b4e2ad7c98ca51e66c54ef9662e2bd
João Pedro <joao.pedro.hsd@gmail.com>
2018-08-04 12:58:38 -0300

    Fix date converter functior

--- src/common/convert-date.ts
+++ src/common/convert-date.ts
@@ -1,4 +1,4 @@
 export function convertDate(date: string): Date {
   const dateFormat: number[] = date.split('-').map((value: string): number => parseInt(value, 10))
-  return new Date(dateFormat[2], dateFormat[1] - 1, dateFormat[0])
+  return new Date(dateFormat[0], dateFormat[1] - 1, dateFormat[2])
 }
